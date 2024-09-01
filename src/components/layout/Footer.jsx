82a34cddca414b5449051144d55a4037fba93b88
andevfront <ayelen.and@hotmail.com>
2024-09-01 18:48:35 -0300

    Footer agregado

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,3 +1,16 @@
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-[#0A1527] py-8 text-center">
+      <p>
+        Designed by{" "}
+        <a
+          className="transition-all duration-300 hover:text-main-500"
+          href="https://github.com/andevfront"
+          target="_blank"
+        >
+          Ayelén
+        </a>
+      </p>
+    </footer>
+  );
 };
