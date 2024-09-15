04c718ccf5af95d1b4ad99b569535bffec883464
andevfront <ayelen.and@hotmail.com>
2024-09-14 21:38:46 -0300

    Información del Curriculum agregado

--- src/views/ResumeSection.jsx
+++ src/views/ResumeSection.jsx
@@ -10,43 +10,18 @@ export const ResumeSection = () => {
         <div className="grid grid-cols-6 gap-6 xl:gap-20">
           <div className="col-span-6 sm:col-span-3">
             <h3 className="mb-5 text-[20px] font-bold text-white">Educación</h3>
-            <div className="group border-b border-l-2 border-b-slate-700 border-l-main-500 bg-[#0d1a31] p-10">
-              <h4 className="relative mb-1 text-[16px] font-bold text-main-500 before:absolute before:-left-[51px] before:top-1 before:h-5 before:w-5 before:rounded-full before:border-2 before:border-main-500 before:bg-[#0d1a31] before:transition-colors before:duration-300 before:content-[''] before:group-hover:bg-main-500">
-                [Nombre del Curso]
-              </h4>
-              <h5 className="mb-3 text-sm font-bold text-white">
-                Plataforma/Centro de Estudio / Fecha de finalización
-              </h5>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Repellat, cupiditate! Inventore perferendis architecto neque
-                incidunt animi cum amet dolor corrupti suscipit sapiente illum.
-              </p>
-            </div>
-            <div className="group border-b border-l-2 border-b-slate-700 border-l-main-500 bg-[#0d1a31] p-10">
-              <h4 className="relative mb-1 text-[16px] font-bold text-main-500 before:absolute before:-left-[51px] before:top-1 before:h-5 before:w-5 before:rounded-full before:border-2 before:border-main-500 before:bg-[#0d1a31] before:transition-colors before:duration-300 before:content-[''] before:group-hover:bg-main-500">
-                [Nombre del Curso]
-              </h4>
-              <h5 className="mb-3 text-sm font-bold text-white">
-                Plataforma/Centro de Estudio / Fecha de finalización
-              </h5>
-              <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Repellat, cupiditate! Inventore perferendis architecto neque
-                incidunt animi cum amet dolor corrupti suscipit sapiente illum.
-              </p>
-            </div>
             <div className="group border-l-2 border-l-main-500 bg-[#0d1a31] p-10">
               <h4 className="relative mb-1 text-[16px] font-bold text-main-500 before:absolute before:-left-[51px] before:top-1 before:h-5 before:w-5 before:rounded-full before:border-2 before:border-main-500 before:bg-[#0d1a31] before:transition-colors before:duration-300 before:content-[''] before:group-hover:bg-main-500">
-                [Nombre del Curso]
+                Diseño Estático de Front-End
               </h4>
               <h5 className="mb-3 text-sm font-bold text-white">
-                Plataforma/Centro de Estudio / Fecha de finalización
+                Universidad Nacional del Nordeste / 2023
               </h5>
               <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Repellat, cupiditate! Inventore perferendis architecto neque
-                incidunt animi cum amet dolor corrupti suscipit sapiente illum.
+                Curso intensivo de Front-End, enfocado en el diseño estático y
+                dinámico de aplicaciones web. Aprendí a estructurar páginas con
+                HTML5 y estilizar con CSS3, integrando interactividad con
+                JavaScript.
               </p>
             </div>
           </div>
@@ -56,41 +31,42 @@ export const ResumeSection = () => {
             </h3>
             <div className="group border-b border-l-2 border-b-slate-700 border-l-main-500 bg-[#0d1a31] p-10">
               <h4 className="relative mb-1 text-[16px] font-bold text-main-500 before:absolute before:-left-[51px] before:top-1 before:h-5 before:w-5 before:rounded-full before:border-2 before:border-main-500 before:bg-[#0d1a31] before:transition-colors before:duration-300 before:content-[''] before:group-hover:bg-main-500">
-                [Título del Puesto]
+                Desarrolladora Web Front-End
               </h4>
               <h5 className="mb-3 text-sm font-bold text-white">
-                Nombre de la Empresa / Fecha de finalización
+                Freelance / Actual
               </h5>
               <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Repellat, cupiditate! Inventore perferendis architecto neque
-                incidunt animi cum amet dolor corrupti suscipit sapiente illum.
+                Diseñé y desarrollé plantillas para eCommerce y landing pages
+                usando HTML, CSS, JavaScript, Tailwind CSS y React. Estas
+                plantillas fueron creadas para ser subidas a Template Monster.
               </p>
             </div>
             <div className="group border-b border-l-2 border-b-slate-700 border-l-main-500 bg-[#0d1a31] p-10">
               <h4 className="relative mb-1 text-[16px] font-bold text-main-500 before:absolute before:-left-[51px] before:top-1 before:h-5 before:w-5 before:rounded-full before:border-2 before:border-main-500 before:bg-[#0d1a31] before:transition-colors before:duration-300 before:content-[''] before:group-hover:bg-main-500">
-                [Título del Puesto]
+                Desarrolladora Web Front-End
               </h4>
               <h5 className="mb-3 text-sm font-bold text-white">
-                Nombre de la Empresa / Fecha de finalización
+                Proyectos Personales / Actual
               </h5>
               <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Repellat, cupiditate! Inventore perferendis architecto neque
-                incidunt animi cum amet dolor corrupti suscipit sapiente illum.
+                Desarrollé proyectos personales con React para expandir mis
+                conocimientos, integrando diversas APIs para crear aplicaciones
+                interactivas y funcionales.
               </p>
             </div>
             <div className="group border-l-2 border-l-main-500 bg-[#0d1a31] p-10">
               <h4 className="relative mb-1 text-[16px] font-bold text-main-500 before:absolute before:-left-[51px] before:top-1 before:h-5 before:w-5 before:rounded-full before:border-2 before:border-main-500 before:bg-[#0d1a31] before:transition-colors before:duration-300 before:content-[''] before:group-hover:bg-main-500">
-                [Nombre del Proyecto]
+                Desarrolladora Web Front-End
               </h4>
               <h5 className="mb-3 text-sm font-bold text-white">
-                Desarrollo Web Personal / Fecha de finalización
+                Trabajo realizado para un hotel en Chile / 2022
               </h5>
               <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Repellat, cupiditate! Inventore perferendis architecto neque
-                incidunt animi cum amet dolor corrupti suscipit sapiente illum.
+                Creé el sitio web informativo para UMA Hotel, transformando un
+                diseño en HTML, TailwindCSS y JavaScript. Colaboré con un
+                desarrollador backend en PHP y Laravel 9 para integrar el diseño
+                y un formulario de reserva con redirección a WhatsApp.
               </p>
             </div>
           </div>
