e7cab30a8afdd4057e90c4c58ee7fa4a93857ada
andevfront <ayelen.and@hotmail.com>
2024-09-14 21:54:01 -0300

    Ajustes de color de fondo en TechIcon

--- src/components/ui/TechIcon.jsx
+++ src/components/ui/TechIcon.jsx
@@ -4,7 +4,8 @@ export const TechIcon = ({ name, color, Icon }) => {
   return (
     <div className="group flex flex-col items-center gap-1">
       <div
-        className={`flex h-14 w-14 items-center justify-center rounded-full bg-[${color}] transition-shadow duration-300 ease-in-out group-hover:shadow-[0_0_10px_${color}]`}
+        className="flex h-14 w-14 items-center justify-center rounded-full"
+        style={{ backgroundColor: color }}
       >
         <Icon className="h-8 w-8 text-white" />
       </div>
